e12357f6e89b5ab9f360a542f3ae9bce84bf530b
valrichter <valenrichter11@gmail.com>
2024-02-25 19:45:46 -0300

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use proto::calculator_server::{Calculator, CalculatorServer};
+use tonic::transport::Server;
+
+mod proto {
+    tonic::include_proto!("calculator");
+}
+
+#[derive(Debug, Default)]
+pub struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationResponse {
+            result: input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let calculator = CalculatorService::default();
+    Server::builder()
+    .add_service(CalculatorServer::new(calculator))
+    .serve(addr)
+    .await?;
+    Ok(())
 }
